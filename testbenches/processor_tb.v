72c9d87161d80dbc8cd84f70e6f8774e622b9dc9
Guraa_97 <sahang3@gmail.com>
2021-06-27 21:13:13 +0530

    add processor test bench

---
+++ testbenches/processor_tb.v
@@ -0,0 +1,33 @@
+`timescale 1ns/1ps  // time-unit = 1 ns, precision = 1 ps
+
+module processor_tb();
+    reg clk = 1'b1;
+    wire [15:0] to_mem;
+
+
+    always
+        begin
+            #10 clk = 1'b0;
+            #10 clk = 1'b1;
+        end
+
+    localparam period = 20; 
+
+    processor dut(
+        .clk(clk),
+        .to_mem(to_mem)
+    );
+
+    initial 
+        begin
+            #period;
+            
+            #1000;
+
+            $stop;
+            
+        end
+
+endmodule
+
+
